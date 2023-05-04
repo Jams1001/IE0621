209dea1608a829c5a5461197dcc7cfcbe225614f
Jams1001 <adantumora90@gmail.com>
2023-05-03 23:35:40 -0600

    Añadiendo interface lista
    Signed-off-by: Jams1001 <adantumora90@gmail.com>

--- tb/tb_core_icarus/tb_top.v
+++ tb/tb_core_icarus/tb_top.v
@@ -1,3 +1,5 @@
+`include "intf.sv"
+
 module tb_top;
 
 reg clk;
@@ -41,6 +43,7 @@ begin
     end
 end
 
+/*
 wire          mem_i_rd_w;
 wire          mem_i_flush_w;
 wire          mem_i_invalidate_w;
@@ -63,6 +66,9 @@ wire          mem_d_accept_w;
 wire          mem_d_ack_w;
 wire          mem_d_error_w;
 wire [ 10:0]  mem_d_resp_tag_w;
+*/
+
+intf intf_0(clk, rst);
 
 riscv_core
 u_dut
@@ -73,33 +79,33 @@ u_dut
     // Inputs
      .clk_i(clk)
     ,.rst_i(rst)
-    ,.mem_d_data_rd_i(mem_d_data_rd_w)
-    ,.mem_d_accept_i(mem_d_accept_w)
-    ,.mem_d_ack_i(mem_d_ack_w)
-    ,.mem_d_error_i(mem_d_error_w)
-    ,.mem_d_resp_tag_i(mem_d_resp_tag_w)
-    ,.mem_i_accept_i(mem_i_accept_w)
-    ,.mem_i_valid_i(mem_i_valid_w)
-    ,.mem_i_error_i(mem_i_error_w)
-    ,.mem_i_inst_i(mem_i_inst_w)
+    ,.mem_d_data_rd_i(intf_0.mem_d_data_rd_w)
+    ,.mem_d_accept_i(intf_0.mem_d_accept_w)
+    ,.mem_d_ack_i(intf_0.mem_d_ack_w)
+    ,.mem_d_error_i(intf_0.mem_d_error_w)
+    ,.mem_d_resp_tag_i(intf_0.mem_d_resp_tag_w)
+    ,.mem_i_accept_i(intf_0.mem_i_accept_w)
+    ,.mem_i_valid_i(intf_0.mem_i_valid_w)
+    ,.mem_i_error_i(intf_0.mem_i_error_w)
+    ,.mem_i_inst_i(intf_0.mem_i_inst_w)
     ,.intr_i(1'b0)
     ,.reset_vector_i(32'h80000000)
     ,.cpu_id_i('b0)
 
     // Outputs
-    ,.mem_d_addr_o(mem_d_addr_w)
-    ,.mem_d_data_wr_o(mem_d_data_wr_w)
-    ,.mem_d_rd_o(mem_d_rd_w)
-    ,.mem_d_wr_o(mem_d_wr_w)
-    ,.mem_d_cacheable_o(mem_d_cacheable_w)
-    ,.mem_d_req_tag_o(mem_d_req_tag_w)
-    ,.mem_d_invalidate_o(mem_d_invalidate_w)
-    ,.mem_d_writeback_o(mem_d_writeback_w)
-    ,.mem_d_flush_o(mem_d_flush_w)
-    ,.mem_i_rd_o(mem_i_rd_w)
-    ,.mem_i_flush_o(mem_i_flush_w)
-    ,.mem_i_invalidate_o(mem_i_invalidate_w)
-    ,.mem_i_pc_o(mem_i_pc_w)
+    ,.mem_d_addr_o(intf_0.mem_d_addr_w)
+    ,.mem_d_data_wr_o(intf_0.mem_d_data_wr_w)
+    ,.mem_d_rd_o(intf_0.mem_d_rd_w)
+    ,.mem_d_wr_o(intf_0.mem_d_wr_w)
+    ,.mem_d_cacheable_o(intf_0.mem_d_cacheable_w)
+    ,.mem_d_req_tag_o(intf_0.mem_d_req_tag_w)
+    ,.mem_d_invalidate_o(intf_0.mem_d_invalidate_w)
+    ,.mem_d_writeback_o(intf_0.mem_d_writeback_w)
+    ,.mem_d_flush_o(intf_0.mem_d_flush_w)
+    ,.mem_i_rd_o(intf_0.mem_i_rd_w)
+    ,.mem_i_flush_o(intf_0.mem_i_flush_w)
+    ,.mem_i_invalidate_o(intf_0.mem_i_invalidate_w)
+    ,.mem_i_pc_o(intf_0.mem_i_pc_w)
 );
 
 tcm_mem
@@ -108,30 +114,30 @@ u_mem
     // Inputs
      .clk_i(clk)
     ,.rst_i(rst)
-    ,.mem_i_rd_i(mem_i_rd_w)
-    ,.mem_i_flush_i(mem_i_flush_w)
-    ,.mem_i_invalidate_i(mem_i_invalidate_w)
-    ,.mem_i_pc_i(mem_i_pc_w)
-    ,.mem_d_addr_i(mem_d_addr_w)
-    ,.mem_d_data_wr_i(mem_d_data_wr_w)
-    ,.mem_d_rd_i(mem_d_rd_w)
-    ,.mem_d_wr_i(mem_d_wr_w)
-    ,.mem_d_cacheable_i(mem_d_cacheable_w)
-    ,.mem_d_req_tag_i(mem_d_req_tag_w)
-    ,.mem_d_invalidate_i(mem_d_invalidate_w)
-    ,.mem_d_writeback_i(mem_d_writeback_w)
-    ,.mem_d_flush_i(mem_d_flush_w)
+    ,.mem_i_rd_i(intf_0.mem_i_rd_w)
+    ,.mem_i_flush_i(intf_0.mem_i_flush_w)
+    ,.mem_i_invalidate_i(intf_0.mem_i_invalidate_w)
+    ,.mem_i_pc_i(intf_0.mem_i_pc_w)
+    ,.mem_d_addr_i(intf_0.mem_d_addr_w)
+    ,.mem_d_data_wr_i(intf_0.mem_d_data_wr_w)
+    ,.mem_d_rd_i(intf_0.mem_d_rd_w)
+    ,.mem_d_wr_i(intf_0.mem_d_wr_w)
+    ,.mem_d_cacheable_i(intf_0.mem_d_cacheable_w)
+    ,.mem_d_req_tag_i(intf_0.mem_d_req_tag_w)
+    ,.mem_d_invalidate_i(intf_0.mem_d_invalidate_w)
+    ,.mem_d_writeback_i(intf_0.mem_d_writeback_w)
+    ,.mem_d_flush_i(intf_0.mem_d_flush_w)
 
     // Outputs
-    ,.mem_i_accept_o(mem_i_accept_w)
-    ,.mem_i_valid_o(mem_i_valid_w)
-    ,.mem_i_error_o(mem_i_error_w)
-    ,.mem_i_inst_o(mem_i_inst_w)
-    ,.mem_d_data_rd_o(mem_d_data_rd_w)
-    ,.mem_d_accept_o(mem_d_accept_w)
-    ,.mem_d_ack_o(mem_d_ack_w)
-    ,.mem_d_error_o(mem_d_error_w)
-    ,.mem_d_resp_tag_o(mem_d_resp_tag_w)
+    ,.mem_i_accept_o(intf_0.mem_i_accept_w)
+    ,.mem_i_valid_o(intf_0.mem_i_valid_w)
+    ,.mem_i_error_o(intf_0.mem_i_error_w)
+    ,.mem_i_inst_o(intf_0.mem_i_inst_w)
+    ,.mem_d_data_rd_o(intf_0.mem_d_data_rd_w)
+    ,.mem_d_accept_o(intf_0.mem_d_accept_w)
+    ,.mem_d_ack_o(intf_0.mem_d_ack_w)
+    ,.mem_d_error_o(intf_0.mem_d_error_w)
+    ,.mem_d_resp_tag_o(intf_0.mem_d_resp_tag_w)
 );
 
 endmodule
