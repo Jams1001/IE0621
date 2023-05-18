a0c9feb282acddc59996469c85fe9c626d588279
Kenny <kennyswat@hotmail.com>
2023-05-17 20:33:12 -0600

    Adding comments to checker

--- tb/tb_core_icarus/monitor.sv
+++ tb/tb_core_icarus/monitor.sv
@@ -15,30 +15,37 @@ class monitor;
     err_count = 0;
     forever
       begin
+        // check whenever this happends
         @ (negedge intf.mem_i_valid_w)
+        // Decode Checking
         $display("======================");
         $display("sb: %d", sb.i);
         $display("cpu.mem_i_inst_w: %b\n",intf.mem_i_inst_w);
         $display("cpu.mem_d_data_rd_w: %b\n",intf.mem_d_data_rd_w);
+        // Checking rs1, rs2, and rd
+        // rs1
         if (intf.rs1_w==sb.result_ref_rs1[sb.i-1]) begin
           $display(" * PASS * cpu.rs1_w: %b  ::  ref_model.rs1_w: %b\n",intf.rs1_w, sb.result_ref_rs1[sb.i-1]);
         end
         else begin
-           $display(" * ERROR * cpu.rs1_w: %b  ::  ref_model.rs1_w: %b\n",intf.rs1_w, sb.result_ref_rs1[sb.i-1]);
-           err_count++;
-        end
-        if (intf.rs2_w==sb.result_ref_rs2[sb.i-1]) begin
-          $display(" * PASS * rs2_w: %b\n  ::    ref_model.rs2_w: %b\n",intf.rs2_w, sb.result_ref_rs2[sb.i-1]);
-        end
-        else begin
-          $display(" * ERROR * rs2_w: %b\n  ::    ref_model.rs2_w: %b\n",intf.rs2_w, sb.result_ref_rs2[sb.i-1]);
+          $display(" * ERROR * cpu.rs1_w: %b  ::  ref_model.rs1_w: %b\n",intf.rs1_w, sb.result_ref_rs1[sb.i-1]);
           err_count++;
         end
-        if (intf.rd_w==sb.result_ref_rd[sb.i-1]) begin
-          $display(" * PASS * rd_w: %b\n   ::    ref_model.rd_w: %b\n",intf.rd_w, sb.result_ref_rd[sb.i-1]);
+        // rs2
+        if (intf.rs2_w==sb.result_ref_rs2[sb.i-1]) begin
+          $display(" * PASS * rs2_w: %b  ::    ref_model.rs2_w: %b\n",intf.rs2_w, sb.result_ref_rs2[sb.i-1]);
         end
         else begin
-          $display(" * ERROR * rd_w: %b\n   ::    ref_model.rd_w: %b\n",intf.rd_w, sb.result_ref_rd[sb.i-1]);
+          $display(" * ERROR * rs2_w: %b  ::    ref_model.rs2_w: %b\n",intf.rs2_w, sb.result_ref_rs2[sb.i-1]);
+          err_count++;
+        end
+        // rd
+        if (intf.rd_w==sb.result_ref_rd[sb.i-1]) begin
+          $display(" * PASS * rd_w: %b   ::    ref_model.rd_w: %b\n",intf.rd_w, sb.result_ref_rd[sb.i-1]);
+        end
+        else begin
+          $display(" * ERROR * rd_w: %b   ::    ref_model.rd_w: %b\n",intf.rd_w, sb.result_ref_rd[sb.i-1]);
+          err_count++;
         end
         $display("======================");
       end
