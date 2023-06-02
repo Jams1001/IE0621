10040ec641bf833f571c052aae8621a6ccf82b4c
Jams1001 <adantumora90@gmail.com>
2023-06-01 21:05:36 -0600

    UVM enviroment

--- tb/tb_core_icarus/environment.sv
+++ tb/tb_core_icarus/environment.sv
@@ -1,28 +1,38 @@
-class environment;
-	// Enviroment blocks
-	driver drvr;
-	scoreboard sb;
-  	monitor mntr;
-  	reference_model ref_model;
+class riscv_env extends uvm_env;
 
-		// Interfaces
-  	virtual interface_1 intf_1;
-	
-		// Constructor
-  	function new(virtual interface_1 intf_1);
-    	$display("Creating environment");
-			// obtaining intf_1 from test
-    	this.intf_1 = intf_1;
-				// Initializing enviroment blocks
-      	sb = new();	
-      	ref_model = new(sb);
-      	drvr = new(intf_1, ref_model);
-      	mntr = new(intf_1, sb);
-			// fork to check concurrently
-    	fork 
-    	  mntr.check_decode();
-          mntr.check_initial();
-    	join_none
-  	endfunction
-           
+  `uvm_component_utils(riscv_env)
+
+  function new (string name = "riscv_env", uvm_component parent = null);
+    super.new (name, parent);
+  endfunction
+  
+  virtual intf1 riscv_intf;
+  riscv_agent_active riscv_ag_active;
+  riscv_agent_passive riscv_ag_passive;
+  riscv_scoreboard riscv_sb;
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    
+    if(uvm_config_db #(virtual intf1)::get(this, "", "VIRTUAL_INTERFACE", riscv_intf) == 0) begin
+      `uvm_fatal("INTERFACE_CONNECT", "Could not get from the database the virtual interface for the TB")
+    end
+    
+    riscv_ag_active = riscv_agent_active::type_id::create ("riscv_ag_active", this);
+    riscv_ag_passive = riscv_agent_passive::type_id::create ("riscv_ag_passive", this);
+    riscv_sb = riscv_scoreboard::type_id::create ("riscv_sb", this); 
+    
+    //uvm_config_db #(virtual fifo_intf)::set (null, "uvm_test_top.*", "VIRTUAL_INTERFACE", intf);    
+      
+    uvm_report_info(get_full_name(),"End_of_build_phase", UVM_LOW);
+    print();
+
+  endfunction
+
+  virtual function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    riscv_ag_active.riscv_mntr_wr.mon_analysis_port.connect(riscv_sb.riscv_drv);
+    riscv_ag_passive.riscv_mntr_rd.mon_analysis_port.connect(riscv_sb.riscv_mon);
+  endfunction
+
 endclass
