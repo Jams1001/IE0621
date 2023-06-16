19f7fa16b084eb9dcb5a91e6236a75eaf3d133e4
Jams1001 <adantumora90@gmail.com>
2023-06-15 21:04:04 -0600

    se hizo gran parte driver, falta el sequencer xd
    Signed-off-by: Jams1001 <adantumora90@gmail.com>

--- tb/tb_core_icarus/driver.sv
+++ tb/tb_core_icarus/driver.sv
@@ -61,7 +61,7 @@ endclass
 
 class inst_base_item extends uvm_sequence_item;
 
-  rand logic [6:0] funct7;
+  logic [6:0] funct7;
   rand logic [4:0] rs2;
   rand logic [4:0] rs1;
   rand logic [2:0] funct3;
@@ -72,7 +72,15 @@ class inst_base_item extends uvm_sequence_item;
   logic [3:0][7:0] init_instr;
 
   `uvm_object_utils_begin(inst_base_item)
-    `uvm_field_int (data, UVM_DEFAULT)
+    `uvm_field_int (funct7, UVM_DEFAULT)
+    `uvm_field_int (rs1, UVM_DEFAULT)
+    `uvm_field_int (rs2, UVM_DEFAULT)
+    `uvm_field_int (funct3, UVM_DEFAULT)
+    `uvm_field_int (rd, UVM_DEFAULT)
+    `uvm_field_int (opcode, UVM_DEFAULT)
+    `uvm_field_int (imm, UVM_DEFAULT)
+    `uvm_field_int (instr, UVM_DEFAULT)
+    `uvm_field_int (init_instr, UVM_DEFAULT)
   `uvm_object_utils_end
 
   function new(string name = "riscv_item");
@@ -81,27 +89,22 @@ class inst_base_item extends uvm_sequence_item;
 endclass
 
 
+
 class r_item extends inst_base_item;
-
-  rand logic [7:0] data;
-
-
-
   function new(string name = "r_item");
     super.new(name);
+    opcode; = 7'b0110011;  // opcode for R type
+    instr = {imm, rs1, funct3, rd, opcode};
   endfunction
 endclass
 
 
 class i_item extends inst_base_item;
-
-  rand logic [7:0] data;
-
-  function new(string name = "i_item");
+  function new(string name = "r_item");
     super.new(name);
+    opcode = 7'b0010011;  // opcode for I type
   endfunction
 endclass
-
 
 
 class gen_item_seq extends uvm_sequence;
@@ -116,21 +119,30 @@ class gen_item_seq extends uvm_sequence;
 
   virtual task body();
     r_item r_item = r_item::type_id::create("r_item");
-    i_item i_item = i_item::type_id::create("r_item");
-    j_item j_item = j_item::type_id::create("r_item");
-
+    i_item i_item = i_item::type_id::create("i_item");
     
     for (int i = 0; i < num; i ++) begin
-        start_item(r_item);
-    	r_item.randomize();
-    	`uvm_info("SEQ", $sformatf("Generate new item: "), UVM_LOW)
-    	r_item.print();
-        finish_item(r_item);
-        //`uvm_do(r_item);
+      start_item(r_item);
+      start_item(i_item);
+      if ($random()%2 == 0) begin
+    	  r_item.randomize();
+        `uvm_info("SEQ", $sformatf("Generate new item: "), UVM_LOW)
+    	  r_item.print();
+      finish_item(r_item);
+      end else begin
+        i_item.randomize();
+      `uvm_info("SEQ", $sformatf("Generate new item: "), UVM_LOW)
+    	i_item.print();
+      finish_item(r_item);
+      end
     end
     `uvm_info("SEQ", $sformatf("Done generation of %0d items", num), UVM_LOW)
   endtask
+
 endclass
+
+
+
 
 
 class riscv_driver extends uvm_driver #(riscv_item);
@@ -149,36 +161,44 @@ class riscv_driver extends uvm_driver #(riscv_item);
      end
    endfunction
    
-   virtual function void connect_phase(uvm_phase phase);
+  virtual function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
-    
   endfunction
 
   virtual task run_phase(uvm_phase phase);
     super.run_phase(phase);
     forever begin
       riscv_item r_item;
+      riscv_item i_item;
+
       `uvm_info("DRV", $sformatf("Wait for item from sequencer"), UVM_LOW)
       seq_item_port.get_next_item(r_item);
-      fork
-        write_riscv(r_item);
-      join
+
+      `uvm_info("DRV", $sformatf("Wait for item from sequencer"), UVM_LOW)
+      seq_item_port.get_next_item(i_item);
+
+
+      write_riscv(r_item.instr);
+      write_riscv(i_item.instr);
       seq_item_port.item_done();
     end
   endtask  
 
+
   virtual task write_riscv(riscv_item r_item);
-    if(r_item.rd ==0)   
-      begin
-        @ (negedge riscv_intf.clk);
-        $display("Driving 0x%h value in the DUT\n", r_item.data);
-        riscv_intf.data_in = r_item.data; // Drive to DUT
-        riscv_intf.wr_en = 1;
-        @ (negedge riscv_intf.clk);
-        riscv_intf.wr_en = 0;
-      end
+    for(k=0; k<4; k=k+1) begin
+      mem[ind] = instruction[k];
+      tb_top.u_mem.write(ind, mem[ind]);
+      ind++;
+    end
   endtask
        
+
+
+
+
+
+
   virtual task riscv_reset();  // Reset method
     display("Reset\n");
     riscv_intf.rst = 1;
