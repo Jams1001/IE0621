e1dfc504110db9530854d1612b9ba0f9ae2c9e88
Kenny <kennyswat@hotmail.com>
2023-06-18 14:44:40 -0600

    Cambiando fifo por riscv de scoreboard, no hubo cambio de logica aun

--- tb/tb_core_icarus/scoreboard.sv
+++ tb/tb_core_icarus/scoreboard.sv
@@ -1,19 +1,44 @@
-class scoreboard;
-  logic [999:0][4:0] result_ref_rd;
-  logic [999:0][4:0] result_ref_rs1;
-  logic [999:0][4:0] result_ref_rs2;
-  logic [999:0][6:0] result_ref_opcode;
-  logic [999:0][2:0] result_ref_func_3;
-  logic [999:0][6:0] result_ref_func_7;
-  integer i = 0;
-  function void get_result(logic [4:0] rs2, rs1, rd, [6:0] opcode, [2:0] func_3, [6:0] func_7);
-    result_ref_rs2[i] = rs2;
-    result_ref_rs1[i] = rs1;
-    result_ref_rd[i] = rd;
-    result_ref_opcode[i] = opcode;
-    result_ref_func_3[i] = func_3;
-    result_ref_func_7[i] = func_7;
-    i=i+1;
-  endfunction
-endclass
+`uvm_analysis_imp_decl( _drv )
+`uvm_analysis_imp_decl( _mon ) 
 
+class riscv_scoreboard extends uvm_scoreboard;
+    `uvm_component_utils (riscv_scoreboard)
+
+    function new (string name, uvm_component parent=null);
+		super.new (name, parent);
+	endfunction
+
+    uvm_analysis_imp_drv #(riscv_item, riscv_scoreboard) riscv_drv;
+    uvm_analysis_imp_mon #(riscv_item, riscv_scoreboard) riscv_mon;
+
+    logic [7:0] ref_model [$];  
+  
+	function void build_phase (uvm_phase phase);
+      riscv_drv = new ("riscv_drv", this);
+      riscv_mon = new ("riscv_mon", this);
+	endfunction
+
+    virtual function void write_drv (riscv_item item);
+      `uvm_info ("drv", $sformatf("Data received = 0x%0h", item.data), UVM_MEDIUM)
+      ref_model.push_back(item.data);
+	endfunction
+  
+    virtual function void write_mon (riscv_item item);
+      `uvm_info ("mon", $sformatf("Data received = 0x%0h", item.data), UVM_MEDIUM)
+      if (item.data !== ref_model.pop_front()) begin
+        `uvm_error("SB error", "Data mismatch");
+      end
+      else begin
+        `uvm_info("SB PASS", $sformatf("Data received = 0x%0h", item.data), UVM_MEDIUM);
+      end
+    endfunction
+
+	virtual task run_phase (uvm_phase phase);
+		
+	endtask
+
+	virtual function void check_phase (uvm_phase phase);
+      if(ref_model.size() > 0)
+        `uvm_warning("SB Warn", $sformatf("riscv not empty at check phase. riscv still has 0x%0h data items allocated", ref_model.size()));
+	endfunction
+endclass
